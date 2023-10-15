c085087e66d6d7169215b14ff6ae2abd7cf7436a
Bruno Dias <brunov378@gmail.com>
2023-10-14 23:41:23 -0300

    Adicionar hashing as senhas

--- src/main/java/br/com/brunodias/todolist/user/UserController.java
+++ src/main/java/br/com/brunodias/todolist/user/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -17,10 +19,17 @@ public class UserController {
     
     @PostMapping("/")
     public ResponseEntity create(@RequestBody UserModel userModel){
+
         UserModel user = this.userRepository.findByUsername(userModel.getUsername());
         if (user != null){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe!");
         }
+
+        String passwordHashed = BCrypt.withDefaults()
+        .hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashed);
+
         UserModel userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
